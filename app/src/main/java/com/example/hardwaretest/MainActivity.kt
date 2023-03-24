aca138153625ee837631de4028622b3bd9c334e2
Crl-Vlz <crlvlz0215@gmail.com>
2023-03-24 14:09:55 -0600

    Finished debugging the location feature, have basic functionality running

--- app/src/main/java/com/example/hardwaretest/MainActivity.kt
+++ app/src/main/java/com/example/hardwaretest/MainActivity.kt
@@ -23,17 +23,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var btnCamera : Button = findViewById(R.id.btnCamera)
-        var btnLocation : Button = findViewById(R.id.btnLocation)
-        var txtLocation : TextView = findViewById(R.id.txtLocation)
+        val btnLocation : Button = findViewById(R.id.btnLocation)
+        val txtLocation : TextView = findViewById(R.id.txtLocation)
 
         //Initializes the location manager
         lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        ll = LocationListener { p0 -> txtLocation.text = "Latitude: ${p0.latitude} Longitude: ${p0.longitude}" }
+        ll = LocationListener { p0 -> txtLocation.text = "Latitude: ${String.format("%.4f", p0.latitude)}Longitude: ${String.format("%.2f", p0.longitude)}" }
 
 
         btnLocation.setOnClickListener{
             if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
                 lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, ll)
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1)
             }
         }
 
@@ -47,23 +49,6 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == 1) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.checkSelfPermission(
-                        this,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                        this,
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return
-                }
                 lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, ll)
             }
         }
